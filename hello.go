f6cd8dbffa611db9faa71669f635e11f844f75c9
Duncan Smith <hello@duncanmsmith.com>
2014-03-31 21:21:51 -0500

    Delicious data storage (kinda)

--- hello.go
+++ hello.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-martini/martini"
+)
+
+type Database map[string]string
 
 func main() {
-  fmt.Printf("Hello, world.\n")
+	m := martini.Classic()
+	db := Database{}
+
+	m.Map(db)
+
+	routes(m)
+
+	m.Run()
+}
+
+func routes(m *martini.ClassicMartini) {
+
+	m.Get("/", func() string {
+		return "Hello World!"
+	})
+
+	m.Get("/store/:key/:value", func(p martini.Params, db Database) string {
+		key := p["key"]
+		value := p["value"]
+		db[key] = value
+
+		return "Stored " + value + " in " + key
+	})
+
+	m.Get("/get/:key", func(p martini.Params, db Database) string {
+		key := p["key"]
+		value := db[key]
+		return value
+	})
+
+	m.Get("/hello/:name", func(p martini.Params) string {
+		return "Hello " + p["name"]
+	})
+
 }
